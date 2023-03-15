90b10b69ed3d8692f292c3c6b2cb6d65589a85bf
Violah Chesang <chesangv21@gmail.com>
2023-03-15 17:33:35 +0300

    set the get routes

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const express = require('express');
+const loginRouter = require('./routes/login/index');
 const ejs = require('ejs');
-
 const app = express();
+
+//globall middlewares
+app.use(express.json);
+app.use(express.urlencoded({extended: true}))
+
+app.use('/login-form', loginRouter);
 
 //specify the type of ejs engine
 app.set('view engine', 'ejs');
 
 //render the login form to the browser
-app.get('/login-form', (req, res) => {
-    res.render('login');
+app.get('/login-form', (req, res) => {// route the client accesses
+    res.render('login');//render ejs file
+    
 });
-
 
 //listening port
 const PORT = 4000;
